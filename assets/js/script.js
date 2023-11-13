97eeaf1c22321785f66e8db388fc6e03d8451003
Diegogete <ddrsilva@senacrs.com.br>
2023-11-13 14:51:22 -0300

    AJUSTE m1

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,31 @@ $(document).ready(function() {
             eu.addClass("visualizado");
         }, 250)
     })
+    const modalfilosofia = document.getElementById('modal-filosofia');
+    const modalsociologia = document.getElementById('modal-sociologia');
+    const modalgeografia = document.getElementById('modal-geografia');
+    const modalhistoria = document.getElementById('modal-historia');
+
+    modalfilosofia.addEventListener('hide.bs.modal', () => {
+        hideVideos();
+    })
+    modalsociologia.addEventListener('hide.bs.modal', () => {
+        hideVideos();
+    })
+    modalgeografia.addEventListener('hide.bs.modal', () => {
+        hideVideos();
+    })
+    modalhistoria.addEventListener('hide.bs.modal', () => {
+        hideVideos();
+    })
 })
+function hideVideos (){
+    const stopVideos = () => {
+        document.querySelectorAll('iframe').forEach(v => { v.src = v.src });
+        document.querySelectorAll('video').forEach(v => { v.pause() });
+      };
+      stopVideos();
+}
 
 $('.painel-container button').hover(function (){
     $(this).css('transform', 'scale(2)')
